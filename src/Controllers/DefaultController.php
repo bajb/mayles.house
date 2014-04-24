06b773aaf71e7d05791f7a4ec57ef2a588d415e1
Brooke Bryan <brooke@bajb.net>
2014-04-24 13:43:17 +0100

    Background Image

---
+++ src/Controllers/DefaultController.php
@@ -0,0 +1,12 @@
+<?php
+namespace BajbNet\MaylesHouse\Controllers;
+
+use Cubex\View\LayoutController;
+
+class DefaultController extends LayoutController
+{
+  public function defaultAction()
+  {
+    return '';
+  }
+}
